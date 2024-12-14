c3e4a3032cbfba7a9536d54c701b488de447e405
dinhvune12 <dunhvu47@gmail.com>
2024-12-15 00:53:27 +0700

    them visual

---
+++ WindowsFormsApp10/RibbonForm1.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.XtraBars;
+using DevExpress.XtraTabbedMdi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class RibbonForm1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public RibbonForm1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void barButtonItem1_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            XtraForm1 cc= new XtraForm1();
+           
+           
+            cc.MdiParent=this;
+
+            cc.Show();
+        }
+
+        private void RibbonForm1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
